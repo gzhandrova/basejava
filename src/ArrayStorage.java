67737876b75fcfb2a2370233e1814055e6b93210
ga <gzhandrova@gmail.com>
2022-12-24 17:07:18 +0700

    first commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,42 @@
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+Resume[] storage = new Resume[ 10000 ];
 
-    void clear() {
+void clear () {
+    for (int i = 0; i < storage.length; i++) {
+        if (storage[ i ] != null) {
+            storage[ i ] = null;
+        }
+    }
+}
+
+void save (Resume r) {
+
+}
+Resume get (String uuid) {
+    for (Resume resume : storage) {
+        if (Objects.equals (uuid, resume.uuid)) {
+            return resume;
+        }
+    }
+    return null;
+}
+
+void delete (String uuid) {
+
     }
 
-    void save(Resume r) {
+/**
+ * @return array, contains only Resumes in storage (without null)
+ */
+Resume[] getAll () {
+    return new Resume[0];
     }
-
-    Resume get(String uuid) {
-        return null;
-    }
-
-    void delete(String uuid) {
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
-
-    int size() {
-        return 0;
+int size () {
+    return storage.length;
     }
 }
