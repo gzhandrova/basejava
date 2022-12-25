1325529cde037a5fb15736ada19a1e4467766221
ga <gzhandrova@gmail.com>
2022-12-25 22:07:41 +0700

    corrected HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,34 +5,31 @@ import java.util.Objects;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-Resume[] storage = new Resume[10000];
+Resume[] storage = new Resume[ 10000 ];
 private int count = 0;
 
 void clear () {
-    Arrays.fill (storage, null);
-    count = 0;
+    
+    Arrays.fill (storage, 0, count, null); count = 0;
 }
 
 void save (Resume r) {
-    storage[count] = r;
-    count++;
+    storage[ count ] = r; count++;
 }
+
 Resume get (String uuid) {
-    int uid = 0;
-    for ( int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++) {
         if (Objects.equals (uuid, storage[ i ].getUuid ())) {
-            uid = i;
+            int uid = i; return storage[ uid ];
         }
-    }
-    return storage[uid];
+    } return null;
 }
 
 void delete (String uuid) {
-    for ( int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++) {
         if (Objects.equals (uuid, storage[ i ].getUuid ())) {
-            storage[i] = storage[count-1];
-            storage[count-1] = null;
-            count--;
+            storage[ i ] = storage[ count - 1 ]; storage[ count - 1 ] = null; count--; break;
+            
         }
     }
 }
@@ -41,12 +38,11 @@ void delete (String uuid) {
  * @return array, contains only Resumes in storage (without null)
  */
 Resume[] getAll () {
-    Resume [] allResume = new Resume[count];
-    System.arraycopy (storage, 0, allResume, 0, count);
-    return allResume;
+    Resume[] allResume = new Resume[ count ]; System.arraycopy (storage, 0, allResume, 0, count); return allResume;
 }
-    
+
 int size () {
     return count;
-    }
 }
+}
+
